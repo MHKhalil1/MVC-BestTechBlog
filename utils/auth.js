1f35e674d6a7c9a8e6e965937160467e39e8257a
MHKhalil1 <khalilmahmoud2333@gmail.com>
2022-11-23 14:01:31 -0500

    auth.js/helper.js completed

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const withAuth = (req, res, next) => {
+// If not logged in continue to login page
+    if(!req.session.user_id) {
+        res.redirect('/login');
+// Continue if logged in
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
